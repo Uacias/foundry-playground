f9646cdecd29c9d10704020ebf52b0a610c97b60
Uacias <filgum0326@gmail.com>
2024-03-18 23:45:53 +0100

    add approvals mapping

--- src/EmContract.sol
+++ src/EmContract.sol
@@ -4,12 +4,19 @@ pragma solidity ^0.8.13;
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract MyContract {
+    mapping(address => mapping(address => bool)) public approvals;
+
+    function approve(address _wallet) public {
+        approvals[msg.sender][_wallet] = true;
+    }
+
     function transferToken(
         IERC20 token,
         address _from,
         address _to,
         uint256 _amount
     ) public {
+        require(approvals[msg.sender][_from], "You do not have approval to operate this wallet");
         require(token.transferFrom(_from, _to, _amount), "Transfer failed");
     }
 }
--- test/EmContract.t.sol
+++ test/EmContract.t.sol
@@ -8,10 +8,12 @@ import {MyContract} from "../src/EmContract.sol";
 contract MyContractTest is Test {
     MyContract public myContract;
     IERC20 public token;
+    address public loggedUser;
     address public from;
     address public to;
 
     function setUp() public {
+        loggedUser = address(0xd1405fE8FaEe965075a6F903d773194463da0CfB);
         from = address(0x00000000219ab540356cBB839Cbe05303d7705Fa);
         to = address(0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8);
         token = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
@@ -21,18 +23,25 @@ contract MyContractTest is Test {
     function testTransferToken() public {
         uint256 transferAmount = 66;
         uint256 initialBalance = token.balanceOf(to);
+        vm.prank(loggedUser);
+        myContract.approve(from);
         vm.prank(from);
         token.approve(address(myContract), transferAmount);
+        vm.prank(loggedUser);
         myContract.transferToken(token, from, to, transferAmount);
-
+        vm.stopPrank();
         assertEq(token.balanceOf(to), initialBalance + transferAmount);
     }
 
-    function testNonApproveTransferToken() public {
+    function testTransferWithoutApproval() public {
         uint256 transferAmount = 66;
-        uint256 initialBalance = token.balanceOf(to);
-        vm.expectRevert();        
+        vm.expectRevert();
         myContract.transferToken(token, from, to, transferAmount);
+    }
 
+    function testInsufficientBalance() public {
+        uint256 transferAmount = token.balanceOf(from) + 1;
+        vm.expectRevert();
+        myContract.transferToken(token, from, to, transferAmount);
     }
 }
