9cc6a653b5d13675a379d24cd24eb50f553bd7a8
Uacias <filgum0326@gmail.com>
2024-03-18 18:52:15 +0100

    openzeppelin

--- test/EmContract.t.sol
+++ test/EmContract.t.sol
@@ -2,19 +2,19 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import {Token} from "../src/EmContract.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {MyContract} from "../src/EmContract.sol";
 
 contract MyContractTest is Test {
     MyContract public myContract;
-    Token public token;
+    IERC20 public token;
     address public from;
     address public to;
 
     function setUp() public {
         from = address(0x00000000219ab540356cBB839Cbe05303d7705Fa);
         to = address(0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8);
-        token = Token(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+        token = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
         myContract = new MyContract();
     }
 
